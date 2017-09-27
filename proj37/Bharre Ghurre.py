867c10b957caf1858e2ad9412840e635eff83588
Aniruddha Karajgi <akarajgi0@gmail.com>
2017-09-27 19:18:29 +0530

    part2

--- proj37/Bharre Ghurre.py
+++ proj37/Bharre Ghurre.py
@@ -1,36 +1,103 @@
-import random
+# importing the required modules
 
+import random
+import pyttsx3
+import speech_recognition as sr
 
 i = random.randint(1000, 9999)
 attempts = 1
 
+# printing out instructions
 print(" \t Bharre Gurre: The Guessing Game \n  ")
 print(" \tInstructions \n")
 print("For every digit that you guess correctly in the correct place, you have a 'bharre'.")
 print("For every digit you guess correctly but in the wrong place, you get a 'gurre'.")
-print("Every time the you make a guess, your number of attempts will be shown")
+print("Every time the you make a guess, your number of attempts will be shown.")
 print("Once you guess the entire number correctly, the game is over. \n")
+
+
+# reading out instructions
+
+engine = pyttsx3.init()
+engine.setProperty('rate', 1000)
+engine.say("For every digit that you guess correctly in the correct place, you have a 'bharrey'")
+engine.say("For every digit you guess correctly but in the wrong place, you get a 'ghoorrey'.")
+engine.say("Every time you make a guess, your number of attempts will be shown.")
+engine.say("Once you guess the entire number correctly, the game is over.")
+
+engine.runAndWait()
 
 
 while True:
     try:
         print(" \t Bharre Gurre: The Guessing Game \n  ")
 
-        print("Attempt Number: ", attempts)
+        print("Attempt Number:", attempts)
 
-        attempts += 1
         bharre = []
         gurre = []
 
-        num = int(input("Please guess a four digit number \n Enter 0 to play another game \n"))
+        engine = pyttsx3.init()
+        engine.setProperty('rate', 150)
+        engine.say("Attempt Number: " + str(attempts))
+        engine.say("Please guess a four digit number. Enter 0 to play another game.")
+
+        # using text to speech to start game with user input
+
+        engine.runAndWait()
+        r = sr.Recognizer()
+
+        engine = pyttsx3.init()
+        engine.setProperty('rate', 150)
+        engine.say("Should we begin?")
+
+        engine.runAndWait()
+
+        a = input("Enter 'yes' to start.")
+
+        if a == 'yes':
+
+            # using speech recognition
+            with sr.Microphone() as source:
+                print("Speak:")
+                engine = pyttsx3.init()
+                engine.setProperty('rate', 150)
+                engine.say("Speak.")
+
+                engine.runAndWait()
+
+                audio = r.listen(source)
+
+            try:
+                engine = pyttsx3.init()
+                engine.setProperty('rate', 140)
+                engine.say("Your number is" + r.recognize_google(audio))
+
+                engine.runAndWait()
+
+                print(r.recognize_google(audio))
+            except sr.UnknownValueError:
+                print("Could not understand audio")
+            except sr.RequestError as e:
+                print("Could not request results; {0}".format(e))
+
+            num = int(r.recognize_google(audio))
+
+        attempts += 1
+
         a = list(str(num))
         b = list(str(i))
 
+        # basic code for the game
         if num == 0:
             break
 
         if len(a) < 4 or len(a) > 4:
             print("Please enter a four digit number")
+            engine = pyttsx3.init()
+            engine.setProperty('rate', 150)
+            engine.say("Please enter a four digit number")
+            engine.runAndWait()
 
         for x in a:
             if b[a.index(x)] == x:
@@ -42,12 +109,27 @@ while True:
                 # print(gurre)
 
         if len(bharre) == len(a):
-            print("You got lucky")
+            print("You got lucky!!")
+
+            engine = pyttsx3.init()
+            engine.setProperty('rate', 150)
+            engine.say("You got lucky!!")
+            engine.runAndWait()
             break
 
         print(str(len(bharre)) + "Bharre   " + str(len(gurre)) + "Gurre")
+
+        engine = pyttsx3.init()
+        engine.setProperty('rate', 150)
+        engine.say(str(len(bharre)) + "Bharre   " + str(len(gurre)) + "Gurre")
+        engine.runAndWait()
         print(" \n")
 
+    # Errors for string input and the like
     except ValueError:
         print("Don't act like an idiot and enter a number")
+        engine = pyttsx3.init()
+        engine.setProperty('rate', 150)
+        engine.say("Don't act like an idiot, and enter a number.")
+        engine.runAndWait()
         print(" \n")
