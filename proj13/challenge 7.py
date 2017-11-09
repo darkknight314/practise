34124dbc8f3da020d916172e614fba970f81a2ff
Aniruddha Karajgi <31214064+Polaris000@users.noreply.github.com>
2017-11-09 13:39:13 +0530

    Update challenge 7.py

--- proj13/challenge 7.py
+++ proj13/challenge 7.py
@@ -1,14 +1,14 @@
 import random
 
 # creating a random list
-bunti = []
+random_list = []
 for i in range(10):
-    bunti.append(random.randrange(1, 100, 1))
-print(bunti)
+    random_list.append(random.randrange(1, 100, 1))
+print(random_list)
 
 # printing even elements
-soi = []
-for bubli in bunti:
-    if bubli % 2 == 0:
-        soi.append(bubli)
-print(soi)
+even_terms = []
+for item in random_list:
+    if item % 2 == 0:
+       even_terms.append(item)
+print(even_terms)
